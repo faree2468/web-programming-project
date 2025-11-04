1b0bcde6e735865b1ccb593934f2de8fd250128a
faree2468 <faris.kadric@stu.ibu.edu.ba>
2025-11-04 10:25:04 +0100

    change folder structure

--- backend/index.php
+++ backend/index.php
@@ -1,10 +1,10 @@
 <?php
 require 'vendor/autoload.php';
-require_once __DIR__ . '/services/UserService.php';
-require_once __DIR__ . '/services/BillService.php';
-require_once __DIR__ . '/services/CategoryService.php';
-require_once __DIR__ . '/services/PaymentService.php';
-require_once __DIR__ . '/services/PaymentMethodService.php';
+require_once __DIR__ . '/rest/services/UserService.php';
+require_once __DIR__ . '/rest/services/BillService.php';
+require_once __DIR__ . '/rest/services/CategoryService.php';
+require_once __DIR__ . '/rest/services/PaymentService.php';
+require_once __DIR__ . '/rest/services/PaymentMethodService.php';
 
 
 Flight::register('user_service', 'UserService');
@@ -14,11 +14,11 @@ Flight::register('payment_service', 'PaymentService');
 Flight::register('payment_method_service', 'PaymentMethodService');
 
 
-require_once __DIR__ . '/routes/UserRoutes.php';
-require_once __DIR__ . '/routes/BillRoutes.php';
-require_once __DIR__ . '/routes/CategoryRoutes.php';
-require_once __DIR__ . '/routes/PaymentRoutes.php';
-require_once __DIR__ . '/routes/PaymentMethodRoutes.php';
+require_once __DIR__ . '/rest/routes/UserRoutes.php';
+require_once __DIR__ . '/rest/routes/BillRoutes.php';
+require_once __DIR__ . '/rest/routes/CategoryRoutes.php';
+require_once __DIR__ . '/rest/routes/PaymentRoutes.php';
+require_once __DIR__ . '/rest/routes/PaymentMethodRoutes.php';
 
 
 Flight::route('GET /', function() {
